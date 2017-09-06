da014ae30a4a569456d45a6549dba034814753ec
saurabhvmobi <saurabhs@vmokshagroup.com>
2017-09-06 17:05:39 +0530

    commit

---
+++ VMLibDemo/VMLib.swift
@@ -0,0 +1,20 @@
+//
+//  VMLib.swift
+//  VMLibDemo
+//
+//  Created by vmoksha mobility on 06/09/17.
+//  Copyright © 2017 vmoksha mobility. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class frameWorkClass{
+
+
+   // Print("frameWork Method")
+
+
+
+
+}
